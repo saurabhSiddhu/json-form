4f57c066dc30cc1c8085d6f1c40756a7d666b2a5
Sourabh <sourabh.siddhu@gmail.com>
2021-07-03 16:59:19 +0530

    Form builder: added demo renderer

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface FormBuilder {
+        "schema": any;
     }
 }
 declare global {
@@ -22,6 +23,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface FormBuilder {
+        "schema"?: any;
     }
     interface IntrinsicElements {
         "form-builder": FormBuilder;
--- src/components/form-builder/form-builder.tsx
+++ src/components/form-builder/form-builder.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'form-builder',
@@ -6,11 +6,25 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class FormBuilder {
-
+  @Prop() schema;
+  elements;
+  elementMap ={
+    'input': <input value="hello world"/>,
+    'select': <select/>,
+    'radio': <radio/>
+  }
+  componentWillLoad() {
+    this.elements = JSON.parse(this.schema).map((element) => {
+      return this.elementMap[element.type];
+    })
+  }
+  
   render() {
     return (
       <Host>
-        Form builder
+        {
+          this.elements
+        }
       </Host>
     );
   }
